639f5d99ceada2b5aa42bfd3e6095166573238d8
danielVFS <danielv759153@gmail.com>
2022-10-09 13:49:25 -0300

    feat(Post): post content

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,39 @@
-export function Post({ author, content }) {
+import styles from "./Post.module.scss";
+
+export function Post() {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/54604875?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Daniel Vitor</strong>
+            <span>Frontend Developer</span>
+          </div>
+        </div>
+
+        <time title="09 de outubro as 08:15" dateTime="2022-10-09 08:15:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifólio. É um projeto que
+          fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+          DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 }
